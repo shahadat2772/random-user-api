695671907c17e9ba2d15f5932675a859028b91e7
shahadat2772 <shahadat2772@gmailcom>
2022-10-02 10:01:24 +0600

    added the fack user data

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
+const fs = require("fs");
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
 // routes
+fs.readFile("./users.json", (err, data) => {
+  console.log(JSON.parse(data));
+});
 
 // APIs
 app.get("/", (req, res) => {
